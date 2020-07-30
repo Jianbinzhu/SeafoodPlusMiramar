95c608e539ebf6f90b6f775208bf29894da571ea
James Zhu <jianbinzhu0_0@hotmail.com>
2020-07-30 17:19:23 +1200

    Initial commit from Project Skeleton

---
+++ app/src/Page.php
@@ -0,0 +1,18 @@
+<?php
+
+use SilverStripe\CMS\Model\SiteTree;
+
+class Page extends SiteTree
+{
+
+    /**
+     * @var array
+     */
+    private static $db = [];
+
+    /**
+     * @var array
+     */
+    private static $has_one = [];
+
+}
